fa9177877e2cfea54106eff7b06783bd5b7237f1
venkatrag1 <venkatraghavan.r@gmail.com>
2019-07-23 17:21:35 +0000

    P2 complete

--- P2/problem_2-RotatedSortedArraySearch.py
+++ P2/problem_2-RotatedSortedArraySearch.py
@@ -10,7 +10,44 @@ def rotated_array_search(input_list, number):
     Returns:
        int: Index or -1
     """
-   pass
+    return _rotated_array_search(input_list, number, 0, len(input_list)-1)
+
+def _rotated_array_search(input_list, number, left, right):
+    """
+    Find the index by searching in a rotated sorted array
+
+    Args:
+       input_list(array): Input array to search
+       number(int): Target to search
+       left(int):
+       right(int):
+    Returns:
+       int: Index or -1
+    """
+    if left > right:
+        return -1
+    middle = left + (right - left) // 2
+    if input_list[middle] == number:
+        if middle == 0 or input_list[middle-1] != input_list[middle]: # Handling duplicates anyway even though problem states no duplicates
+            return middle
+    if input_list[left] < input_list[middle]: # We know there's no rotation on left side of input_list
+        if input_list[left] <= number <= input_list[middle]: # Since the target is within range of sorted left, search only on left
+            return _rotated_array_search(input_list, number, left, middle-1)
+        else: # We know target doesn't lie in left range, so maybe its on the right side where rotation maybe present
+            return _rotated_array_search(input_list, number, middle+1, right)
+    elif input_list[middle] < input_list[right]: # We know there's no rotation on the right side but rotation did exist on left side
+        if input_list[middle] <= number <= input_list[right]: # Since the target is within range of sorted right, search only on right
+            return _rotated_array_search(input_list, number, middle+1, right)
+        else: # We know target doesn't lie in right range, so maybe its on the rotated left side
+            return _rotated_array_search(input_list, number, left, middle-1)
+    else: # Will only enter here when duplicates are present and so we can't determine which side rotation is on by comparing extremes and middle
+        # Search on both sides - first left, if not found on left then right
+        left_index = _rotated_array_search(input_list, number, left, middle-1) # Search Left
+        if left_index != -1:
+            return left_index
+        return _rotated_array_search(input_list, number, middle+1, right) # Search Right
+
+
 
 def linear_search(input_list, number):
     for index, element in enumerate(input_list):
@@ -26,16 +63,24 @@ def test_function(test_case):
     else:
         print("Fail")
 
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 8])
-test_function([[6, 7, 8, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 10])
-
-
 class TestRotatedArraySearch(unittest.TestCase):
     def setUp(self):
         print("\n\n****{}****".format(self._testMethodName))
 
     def test_case1(self):
-        self.print_pass_fail(3, 9)
+        test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
+
+    def test_case2(self):
+        test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
+
+    def test_case3(self):
+        test_function([[6, 7, 8, 1, 2, 3, 4], 8])
+
+    def test_case4(self):
+        test_function([[6, 7, 8, 1, 2, 3, 4], 1])
+
+    def test_case5(self):
+        test_function([[6, 7, 8, 1, 2, 3, 4], 10])
+
+if __name__ == '__main__':
+    unittest.main()
